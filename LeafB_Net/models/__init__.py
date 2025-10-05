3af69bc95d779315fa13920e97a557836ca999f9
Natsuki-nanami <167593018+Natsuki-nanami@users.noreply.github.com>
2025-10-05 17:21:48 +0800

    Update __init__.py

--- LeafB_Net/models/__init__.py
+++ LeafB_Net/models/__init__.py
@@ -1,4 +1,12 @@
-from .alexnet import *
-from .resnet import *
-from .densenet import *
-from .mobilenet import *
+from .EfficientNet import *
+from .FasterNet import *
+from .Iresnet50 import *
+from .LSNet import *
+from .MobileMamba import *
+from .MobileNetV2 import *
+from .MobileNetV3 import *
+from .MobileVit import *
+from .ResNet50 import *
+from .ShuffleNet import *
+
+
